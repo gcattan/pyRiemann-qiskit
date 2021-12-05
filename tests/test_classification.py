1670a8a87b6b2ab16d7670af1a1d8778445e76a4
gcattan <gregoire.cattan@ibm.com>
2021-12-05 14:46:59 +0100

    - fvt pass

--- tests/test_classification.py
+++ tests/test_classification.py
@@ -12,26 +12,54 @@ rclf = [QuanticVQC, QuanticSVM]
 
 @pytest.mark.parametrize("classif", rclf)
 class QuantumClassifierTestCase:
+    def prepare_data_params(self):
+        self.n_samples = 100
+        samples = self.get_feats(self.n_samples, self.n_features)
+        labels = self.get_labels(self.n_samples, self.n_classes)
+        return samples, labels
+
+    def prepare_data_quantic_svm(self):
+        self.n_samples = 10
+        # We need to have different values for first and second classes
+        # in our samples or vector machine will not converge
+        self.class_len = self.n_samples // self.n_classes  # balanced set
+        samples_0 = np.zeros((self.class_len, self.n_features))
+        samples_1 = np.ones((self.class_len, self.n_features))
+        samples = np.concatenate((samples_0, samples_1), axis=0)
+        labels = self.get_labels(self.n_samples, self.n_classes)
+        return samples, labels
+
+    def prepare_data_quantic_vqc(self):
+        # To achieve testing in a reasonnable amount of time,
+        # we will lower the size of the feature and the number of trials
+        self.n_samples = 4
+        samples = self.get_feats(self.n_samples, self.n_features)
+        labels = self.get_labels(self.n_samples, self.n_classes)
+        return samples, labels
+
     def test_two_classes(self, classif, get_covmats, get_labels, get_feats):
-        n_samples, n_channels, n_classes = 100, 3, 2
-        n_features = n_channels ** 2
-        samples = get_feats(n_samples, n_features)
-        labels = get_labels(n_samples, n_classes)
-        covset = get_covmats(n_samples, n_channels)
-        self.clf_params(classif, covset, labels)
+        self.get_covmats = get_covmats
+        self.get_labels = get_labels
+        self.get_feats = get_feats
+        self.n_channels, self.n_classes = 3, 2
+        self.n_features = self.n_channels ** 2
+
         self.clf_init_with_quantum_false(classif)
         self.clf_init_with_quantum_true(classif)
-        self.clf_split_classes(classif, samples, labels, n_samples,
-                               n_features, n_classes)
-        self.clf_fvt(classif, samples, labels, n_samples, n_features, n_classes)
+
+        samples, labels = self.prepare_data_params()
+        self.clf_params(classif, samples, labels)
+        self.clf_split_classes(classif, samples, labels)
+
+        self.clf_fvt(classif)
 
 
 class TestClassifier(QuantumClassifierTestCase):
-    def clf_params(self, classif, covset, labels):
+    def clf_params(self, classif, samples, labels):
         clf = make_pipeline(XdawnCovariances(), TangentSpace(),
                             classif())
         skf = StratifiedKFold(n_splits=5)
-        cross_val_score(clf, covset, labels, cv=skf, scoring='roc_auc')
+        cross_val_score(clf, samples, labels, cv=skf, scoring='roc_auc')
 
     def clf_init_with_quantum_false(self, classif):
         if classif is QuanticVQC:
@@ -63,50 +91,45 @@ class TestClassifier(QuantumClassifierTestCase):
         with pytest.raises(RequestsApiError):
             q._init_quantum()
 
-    def clf_split_classes(self, classif, samples, labels,
-                          n_samples, n_features, n_classes):
+    def clf_split_classes(self, classif, samples, labels):
         """Test _split_classes method of quantum classifiers"""
         q = classif()
 
         # As fit method is not called here, classes_ is not set.
         # so we need to provide the classes ourselves.
-        q.classes_ = range(0, n_classes)
+        q.classes_ = range(0, self.n_classes)
 
         x_class1, x_class0 = q._split_classes(samples, labels)
-        class_len = n_samples // n_classes  # balanced set
-        assert np.shape(x_class1) == (class_len, n_features)
-        assert np.shape(x_class0) == (class_len, n_features)
+        class_len = self.n_samples // self.n_classes  # balanced set
+        assert np.shape(x_class1) == (class_len, self.n_features)
+        assert np.shape(x_class0) == (class_len, self.n_features)
 
-    def clf_fvt(self, classif, samples, labels, n_samples, n_features, n_classes):
+    def clf_fvt(self, classif): 
         if classif is QuanticSVM:
-            self.clf_fvt_classical_svc(classif, labels, n_samples, n_features, n_classes)
-            self.clf_fvt_quantic_svc(classif, labels, n_samples, n_features, n_classes)
+            samples, labels = self.prepare_data_quantic_svm()
+            self.clf_fvt_classical_svc(classif, samples, labels)
+            self.clf_fvt_quantic_svc(classif, samples, labels)
         else:
+            samples, labels = self.prepare_data_quantic_vqc()
             self.clf_fvt_vqc(classif, samples, labels)
 
-    def clf_fvt_classical_svc(self, classif, labels, n_samples, n_features, n_classes):
+    def clf_fvt_classical_svc(self, classif, samples, labels):
         """ Perform standard SVC test
         (canary test to assess pipeline correctness)
         """
         # When quantum=False, it should use
         # classical SVC implementation from SKlearn
         q = classif(quantum=False, verbose=False)
-        # We need to have different values for first and second classes
-        # in our samples or vector machine will not converge
-        class_len = n_samples // n_classes  # balanced set
-        samples_0 = np.zeros((class_len, n_features))
-        samples_1 = np.ones((class_len, n_features))
-        samples = np.concatenate((samples_0, samples_1), axis=0)
 
         q.fit(samples, labels)
         # This will autodefine testing sets
         prediction = q.predict(samples)
         # In this case, using SVM, predicting accuracy should be 100%
-        assert prediction[:class_len].all() == q.classes_[0]
-        assert prediction[class_len:].all() == q.classes_[1]
+        assert prediction[:self.class_len].all() == q.classes_[0]
+        assert prediction[self.class_len:].all() == q.classes_[1]
 
 
-    def clf_fvt_quantic_svc(self, classif, labels, n_samples, n_features, n_classes):
+    def clf_fvt_quantic_svc(self, classif, samples, labels):
         """Perform SVC on a simulated quantum computer.
         This test can also be run on a real computer by providing a qAccountToken
         To do so, you need to use your own token, by registering on:
@@ -115,21 +138,12 @@ class TestClassifier(QuantumClassifierTestCase):
         """
         # We will use a quantum simulator on the local machine
         q = QuanticSVM(quantum=True, verbose=False)
-        # We need to have different values for target and non-target in our samples
-        # or vector machine will not converge
-        # To achieve testing in a reasonnable amount of time,
-        # we will lower the size of the feature and the number of trials
-        # n_samples, n_features, n_classes = 10, 4, 2
-        class_len = n_samples // n_classes  # balanced set
-        samples_0 = np.zeros((class_len, n_features))
-        samples_1 = np.ones((class_len, n_features))
-        samples = np.concatenate((samples_0, samples_1), axis=0)
 
         q.fit(samples, labels)
         prediction = q.predict(samples)
         # In this case, using SVM, predicting accuracy should be 100%
-        assert prediction[:class_len].all() == q.classes_[0]
-        assert prediction[class_len:].all() == q.classes_[1]
+        assert prediction[:self.class_len].all() == q.classes_[0]
+        assert prediction[self.class_len:].all() == q.classes_[1]
 
 
     def clf_fvt_vqc(self, classif, samples, labels):
@@ -137,10 +151,7 @@ class TestClassifier(QuantumClassifierTestCase):
         # We will use a quantum simulator on the local machine
         # quantum parameter for VQC is always true
         q = classif(verbose=False)
-        # To achieve testing in a reasonnable amount of time,
-        # we will lower the size of the feature and the number of trials
-        # n_samples, n_features, n_classes = 4, 4, 2
-
+        
         q.fit(samples, labels)
         prediction = q.predict(samples)
         # Considering the inputs, this probably make no sense to test accuracy.
