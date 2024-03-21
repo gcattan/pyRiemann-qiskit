846cc248ec40d6670f0fb95bb083baf4e01a9bc4
gcattan <gcattan@hotmail.fr>
2024-03-21 12:32:06 +0100

    Update light_benchmark.py

--- benchmarks/light_benchmark.py
+++ benchmarks/light_benchmark.py
@@ -64,6 +64,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.33, random_state=42, stratify=y
 )
 
+print("y_train, y_test")
 print(y_train, y_test)
 
 ##############################################################################
